d7fe5b52ab9d878506f10316b7a1f260e93fa4b7
Beat Josef Zimmermann <bzimmerm@dxc.com>
2024-10-09 11:35:26 +0200

    Styled
    Added styled method

--- bhjcqr.py
+++ bhjcqr.py
@@ -4,20 +4,42 @@
 # 
 ##########################################################################
 
-version = '0.2 7-Oct-2024'
+version = '0.3'
 
-# install dependencies (one-time)
+# version history
+# 07/10/2024 initial version (basic only)
+# 09/10/2024 added styled method
+
+
+# dependencies (one-time)
 # pip3 install opencv-python qrcode numpy pillow
+# pip install "qrcode[pil]"
 # ########################################################################
 
 
 import qrcode
-# example data
-data = "Beat Zimmermann Socio 00003"
-# output file name
-filename = "socio_qr.png"
+
+# basic method
+
+# input data
+data = "Beat Zimmermann - Socio 00003"
 # generate qr code
 img = qrcode.make(data)
 # save img to a file
 img.save("Beat Zimmermann.png")
 
+
+# styled method
+
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers.pil import RoundedModuleDrawer
+from qrcode.image.styles.colormasks import RadialGradiantColorMask
+
+# input data
+data = "Robert Carnicero - Socio 00001"
+qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+qr.add_data(data)
+
+img = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer(), 
+                    color_mask=RadialGradiantColorMask(), embeded_image_path="logo.png")
+img.save("Styled.png")
